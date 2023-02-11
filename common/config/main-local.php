2aacb1fb7c0082eba179b714776c1599a864c2ca
wcj <1487743280@qq.com>
2023-02-11 19:44:59 +0800

    initial repository

---
+++ common/config/main-local.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=yii2',
+            'username' => 'root',
+            'password' => '123456',
+            'charset' => 'utf8mb4',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+        ],
+    ],
+];
